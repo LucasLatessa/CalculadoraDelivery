b38d924a0fac02a60f59fc168dccdfbabe4404ee
lucaslatessa <lucaslatessa@gmail.com>
2025-04-16 17:02:54 -0300

    Update home.js

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -57,6 +57,7 @@ function Home() {
               tipo_ubicacion: results[0].types.join(", "),
               status: "exitosa",
             };
+            console.log("Geocodificación exitosa:", geocodeData)
             logConsulta(direccion, geocodeData);
             resolve(results[0].geometry.location)
           } else {
